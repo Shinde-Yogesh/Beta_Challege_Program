d25cd191979800df3fa25fd276286d552edbbde3
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-12 22:46:31 +0530

    using replace method

--- src/com/string_program/Valid_IP.java
+++ src/com/string_program/Valid_IP.java
@@ -4,7 +4,7 @@ public class Valid_IP {
 	//base case
 	//A defanged IP address replaces every period "." with "[.]".
 	public static String defangIPaddr(String address) {
-        return address.replaceAll("\\.", "[.]");
+		return address.replace(".", "[.]");
     }
 	public static void main(String[] args) {
 		String str = "1.1.1.1";
