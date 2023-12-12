ab3eb53f50eb4b369fbfcd98c14542de104bee1e
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-12 22:48:26 +0530

    using join method and spilt

--- src/com/string_program/Valid_IP.java
+++ src/com/string_program/Valid_IP.java
@@ -4,7 +4,9 @@ public class Valid_IP {
 	//base case
 	//A defanged IP address replaces every period "." with "[.]".
 	public static String defangIPaddr(String address) {
-		return address.replace(".", "[.]");
+//		return address.replace(".", "[.]");
+//		return address.replaceAll(".", "[.]");
+		return String.join("[.]", address.split("\\."));
     }
 	public static void main(String[] args) {
 		String str = "1.1.1.1";
