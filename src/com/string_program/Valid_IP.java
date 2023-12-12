dca155ac27228e57872f388e3897714f75998c37
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-12 22:50:01 +0530

    using string builder and ternary operator

--- src/com/string_program/Valid_IP.java
+++ src/com/string_program/Valid_IP.java
@@ -6,7 +6,12 @@ public class Valid_IP {
 	public static String defangIPaddr(String address) {
 //		return address.replace(".", "[.]");
 //		return address.replaceAll(".", "[.]");
-		return String.join("[.]", address.split("\\."));
+//		return String.join("[.]", address.split("\\."));
+		StringBuilder sb = new StringBuilder();
+        for (char c : address.toCharArray()) {
+            sb.append(c == '.' ? "[.]" : c);
+        }
+        return sb.toString();
     }
 	public static void main(String[] args) {
 		String str = "1.1.1.1";
